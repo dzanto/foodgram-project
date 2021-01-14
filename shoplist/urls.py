895ed2d2145430ea3a6ab44cdb2295d873bc874f
Anton <dzanto@gmail.com>
2021-01-14 16:52:54 +0300

    editing templates is done

--- shoplist/urls.py
+++ shoplist/urls.py
@@ -3,4 +3,18 @@ from shoplist import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("auth/", views.auth, name="auth"),
+    path("authorrecipe/", views.authorrecipe, name="authorrecipe"),
+    path("changepassword/", views.changepassword, name="changepassword"),
+    path("custompage/", views.custompage, name="custompage"),
+    path("favorite/", views.favorite, name="favorite"),
+    path("formchangerecipe/", views.formchangerecipe, name="formchangerecipe"),
+    path("formrecipe/", views.formrecipe, name="formrecipe"),
+    path("myfollow/", views.myfollow, name="myfollow"),
+    path("reg/", views.reg, name="reg"),
+    path("resetpassword/", views.resetpassword, name="resetpassword"),
+    path("shoplist/", views.shoplist, name="shoplist"),
+    path("singlepage/", views.singlepage, name="singlepage"),
+    path("singlepagenotauth/", views.singlepagenotauth, name="singlepagenotauth"),
 ]
+
--- shoplist/views.py
+++ shoplist/views.py
@@ -6,3 +6,94 @@ def index(request):
         request,
         'indexNotAuth.html'
     )
+
+
+def auth(request):
+    return render(
+        request,
+        'authForm.html'
+    )
+
+
+def authorrecipe(request):
+    return render(
+        request,
+        'authorRecipe.html'
+    )
+
+
+def changepassword(request):
+    return render(
+        request,
+        'changePassword.html'
+    )
+
+
+def custompage(request):
+    return render(
+        request,
+        'customPage.html'
+    )
+
+
+def favorite(request):
+    return render(
+        request,
+        'favorite.html'
+    )
+
+
+def formchangerecipe(request):
+    return render(
+        request,
+        'formChangeRecipe.html'
+    )
+
+
+def formrecipe(request):
+    return render(
+        request,
+        'formRecipe.html'
+    )
+
+
+def myfollow(request):
+    return render(
+        request,
+        'myFollow.html'
+    )
+
+
+def reg(request):
+    return render(
+        request,
+        'reg.html'
+    )
+
+
+def resetpassword(request):
+    return render(
+        request,
+        'resetPassword.html'
+    )
+
+
+def shoplist(request):
+    return render(
+        request,
+        'shopList.html'
+    )
+
+
+def singlepage(request):
+    return render(
+        request,
+        'singlePage.html'
+    )
+
+
+def singlepagenotauth(request):
+    return render(
+        request,
+        'singlePageNotAuth.html'
+    )
