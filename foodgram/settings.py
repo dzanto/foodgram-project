6fd17cfce8f6018ec598c11c44348532b9e50d20
Anton <dzanto@gmail.com>
2021-01-27 16:57:26 +0300

    add tags1

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'shoplist',
     'multiselectfield',
     'rest_framework',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
 ]
 
 ROOT_URLCONF = 'foodgram.urls'
